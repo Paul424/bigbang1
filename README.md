21e80811e80499b60a6e7050bf460a00ac0d1983
Paul Bormans <paul.bormans@kpn.com>
2026-02-27 15:00:43 +0100

    feat: first deploy running

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # bigbang1
 
-Just a test / demo setup to run bigbang
+Just a test / demo setup to run bigbang.
+
+We are using the bigbang quickstart script mostly but since we want to use our own kind setup (and not k3d or aws) we wrap it in our own bash script (run.sh).
 
 # Prerequisites
+
+## Tools and environment
 - go 
 - kind 
 - docker
@@ -10,10 +14,31 @@ Just a test / demo setup to run bigbang
 - k9s
 - kustomize
 - helm
+- flux cli
+- pgp
+- sops
+- jq
+- yq
+
+## Account
+- [github account](https://github.com/)
+- [p1 registry account](https://registry1.dso.mil/)
 
 # Setup
 
+## Kind cluster
+
 Spin a kind cluster:
 ```
+bash ./run.sh up_kind
+```
+
+## Bootstrap bigbang
+
+
+```
+export REGISTRY1_USERNAME=<account-name-to-access-p1-registry>
+export REGISTRY1_TOKEN=<p1-registry-cli-secret>
+bash ./run.sh up_bigbang
 
 ```
--- run.sh
+++ run.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 {up_kind} [naam]"
+    echo "Usage: $0 {up_kind|up_bigbang} [naam]"
     exit 1
 fi
 
+BASE=$(dirname $(realpath $0))
+echo $BASE
 COMMAND="$1"
 shift
 
 function up_kind {
     NAME=${1}
-    kind create cluster --config=kind.yaml --name $NAME
+    # kind create cluster --config=kind.yaml --name $NAME
+    kubectl config view --minify=false --raw=true > ~/.kube/${NAME}-dev-quickstart-config
     # kind delete cluster
+}
+
+function up_bigbang {
+    NAME=${1}
+    export REPO1_LOCATION=$BASE/upstream
+    bash ./quickstart.sh --host $NAME --deploy
 }
 
 
@@ -22,23 +31,15 @@ case "$COMMAND" in
         up_kind $NAME
         ;;
     
-    # stop)
-    #     echo "Stoppen van service..."
-    #     if [ -n "$NAAM" ]; then
-    #         echo "Service naam: $NAAM"
-    #     fi
-    #     ;;
-    
-    # status)
-    #     echo "Status opvragen..."
-    #     if [ -n "$NAAM" ]; then
-    #         echo "Service naam: $NAAM"
-    #     fi
-    #     ;;
+    up_bigbang)
+        NAME=${1:-bb1};
+        shift
+        up_bigbang $NAME
+        ;;
     
     *)
         echo "Invalid argument"
-        echo "Usage: $0 {kind} [naam]"
+        echo "Usage: $0 {up_kind} [naam]"
         exit 1
         ;;
 esac
